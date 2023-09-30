730e99e5f4d52d5b59f9fe2f749af502fc297832
madhavi-chavva <125069098+madhavi-chavva@users.noreply.github.com>
2023-09-29 22:38:00 -0400

    # 26 refactor s3 bucket into the new terrahouse_aws module (#27)

--- main.tf
+++ main.tf
@@ -1,15 +1,26 @@
-# https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
+terraform {
+  #backend "remote" {
+  #  hostname = "app.terraform.io"
+  #  organization = "ExamPro"
 
-
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
-resource "aws_s3_bucket" "website_bucket" {
-  # Bucket Naming Rules
-  #https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html?icmpid=docs_amazons3_console
-  bucket = var.bucket_name
-  
-  tags = {
-    UserUuid = var.user_uuid
-  }
+  #  workspaces {
+  #    name = "terra-house-1"
+  #  }
+  #}
+  # cloud {
+  #   organization = "xyz-terraform"
+  #   workspaces {
+  #     name = "Terra-house-1"
+  #   }
+  # }
 }
+
+module "terrahouse_aws" {
+  source = "./modules/terrahouse_aws"
+  user_uuid = var.user_uuid
+  bucket_name = var.bucket_name
+}
+
+
 
 
